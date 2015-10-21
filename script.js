fea7bff44d22f8e70415e7424e1d395011008f36
DamnedGuy <xom9ak@gmail.com>
2015-10-21 10:34:19 +0300

    добавлена кнопка follow и navbar

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+var a;
+
+function onclickabc(){
+    //var abc = document.getElementById('bttn');
+
+    localStorage.setItem ('a', 1);
+    change();
+    /*abc.style.color ='#FFFFFF';
+     abc.style.background ='#337ab7';*/
+
+}
+if (localStorage.getItem('a')){
+    change();
+}
+
+function change(){
+    var abc = document.getElementById('bttn');
+    abc.style.color ='#FFFFFF';
+    abc.style.background ='#337ab7';
+}
