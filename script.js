8020018763390b558051d8a30a58be1593d92d13
DamnedGuy <xom9ak@gmail.com>
2015-10-23 13:04:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-
 var a;
 
 function onclickabc(){
     //var abc = document.getElementById('bttn');
-    
+
     localStorage.setItem ('a', 1);
     change();
     /*abc.style.color ='#FFFFFF';
      abc.style.background ='#337ab7';*/
 
 }
+if (localStorage.getItem('a')){
+    change();
+}
 
 function change(){
     var abc = document.getElementById('bttn');
-    if (a = 1) {
     abc.style.color ='#FFFFFF';
     abc.style.background ='#337ab7';
-} else {
-    abc.style.color='#FFFFFF';
-    abc.style.background ='#FFFFFF';
 }
 
-if (localStorage.getItem('a')) {
-    change();
-};
-onclick.change ();
+function revert(){
+    var abc = document.getElementById('bttn');
+    abc.style.color ='#FFFFFF';
+    abc.style.background ='#FFFFFF';
+}
