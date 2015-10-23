8790da53ae94a1d9abb55333229f8e48fae1897c
DamnedGuy <xom9ak@gmail.com>
2015-10-23 12:39:29 +0200

    Update script.js
    включение и выкл. кнопки

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ var a;
 
 function onclickabc(){
     //var abc = document.getElementById('bttn');
-
+    
     localStorage.setItem ('a', 1);
     change();
     /*abc.style.color ='#FFFFFF';
      abc.style.background ='#337ab7';*/
 
 }
-if (localStorage.getItem('a')){
-    change();
-}
 
 function change(){
     var abc = document.getElementById('bttn');
+    if (a = 1) {
     abc.style.color ='#FFFFFF';
     abc.style.background ='#337ab7';
+} else {
+    abc.style.color='#FFFFFF';
+    abc.style.background ='#FFFFFF';
+}
+
+if (localStorage.getItem('a')) {
+    change();
 }
